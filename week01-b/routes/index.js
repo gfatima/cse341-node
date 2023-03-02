d940ad1afa3751409d3c3a22c1aa4253a82701ed
Ivette-syta <37168871+gfatima@users.noreply.github.com>
2023-03-02 16:59:34 -0500

    Assignment W1-b v1.8

--- week01-b/routes/index.js
+++ week01-b/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/contacts', require('./contacts'))
+router.use('/', require('./contacts'))
 
 module.exports = router;
