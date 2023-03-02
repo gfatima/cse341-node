9bc2d734ba8dd28a265c361f79cfb18e9a3783e5
Ivette-syta <37168871+gfatima@users.noreply.github.com>
2023-03-02 16:16:59 -0500

    Assignment W1-b v1.8

--- week01-b/controllers/contacts.js
+++ week01-b/controllers/contacts.js
@@ -1,7 +1,7 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
+const getAll = async (req, res, next) => {
     const result = await mongodb.getDb().db("Contacts").collection('contacts').find();
     result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
