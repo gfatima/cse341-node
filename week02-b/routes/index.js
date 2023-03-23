f6005defe0b42eed42cc6ebda397f2548bacd465
Ivette-syta <37168871+gfatima@users.noreply.github.com>
2023-03-23 15:20:34 -0500

    Assignment W2-b v 1.3

--- week02-b/routes/index.js
+++ week02-b/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-// router.get('/', require('./contacts'))
+router.get('/', require('./contacts'))
 router.use('/', require('./swagger'))
 router.use('/contacts', require('./contacts'))
 
